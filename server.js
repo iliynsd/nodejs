be2ecd9428be1b42c684a079588b0d573ebd464f
alex <iliynsd@icloud.com>
2022-05-18 14:18:18 +0300

    change

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 8080
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(port, ()=>{
-    console.log('Exaple app')
-})
+app.listen(3000)
